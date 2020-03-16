a735cacdeca1ffaaec4ad43ec45f43a49bf48a07
Matin Soltany <matintemporary1@gmail.com>
2020-03-16 03:41:11 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {} from "react-native-elements";
+import RNTapsellPlus from "react-native-tapsell-plus";
+
+export default function App() {
+ 
+  return (
+    <View style={styles.container}>
+      <Text>start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
