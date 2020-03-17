c38ef5f470d04b1928910dd9130a3b5f8a141582
Matin Soltany <matintemporary1@gmail.com>
2020-03-17 17:07:09 -0400

    Adding thunk middleware

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,16 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import pointInfoReducer from "../reducers/pointsInfo";
 import userReducer from "../reducers/user";
+import thunk from "redux-thunk";
 
 const store = createStore(
-  combineReducers({
-    pointInfo: pointInfoReducer,
-    user: userReducer
-  })
+  combineReducers(
+    {
+      pointInfo: pointInfoReducer,
+      user: userReducer
+    },
+    applyMiddleware(thunk)
+  )
 );
 
 export default store;
